1f799e3c4578b7b2bbd7cb107a5e4decd2022b16
Aleksandr Sokolovskii <github@amsokol.com>
2018-03-26 14:19:08 +0300

    added prefix data for Operation

--- binary/v1/operation.go
+++ binary/v1/operation.go
@@ -17,11 +17,19 @@ const (
 
 // Operation allows to prepare operation to execute
 type Operation struct {
-	Code int16
-	UID  int64
-	Data bytes.Buffer
+	Code   int16
+	UID    int64
+	Prefix bytes.Buffer
+	Data   bytes.Buffer
 }
 
+// WritePrefix writes data to the prefix buffer.
+// Prefix is sent to the server before data.
+func (o *Operation) WritePrefix(data ...interface{}) error {
+	return write(&o.Prefix, data...)
+}
+
+// Write writes data to the data buffer
 func (o *Operation) Write(data ...interface{}) error {
 	return write(&o.Data, data...)
 }
