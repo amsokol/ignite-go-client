9e9803008b9adf6a09fecd96df89d7074da3f6be
Aleksandr Sokolovskii <github@amsokol.com>
2018-04-25 22:58:58 +0300

    added java HashCode function

---
+++ binary/v1/hash-code_test.go
@@ -0,0 +1,29 @@
+package ignite
+
+import "testing"
+
+func Test_HashCode(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int32
+	}{
+		{
+			name: "success test",
+			args: args{
+				s: "test string",
+			},
+			want: -318923937,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HashCode(tt.args.s); got != tt.want {
+				t.Errorf("hashCode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
