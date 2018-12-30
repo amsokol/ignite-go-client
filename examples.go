6f1276e0076bd3d50f8bf460fba6daf8bfb862d9
Aleksandr Sokolovskii <github@amsokol.io>
2018-12-30 08:37:00 +0300

    #14

---
+++ examples.go
@@ -0,0 +1,4 @@
+package examples
+
+// This is empty file to prevent the following warning of "go get":
+// go build github.com/amsokol/ignite-go-client: no non-test Go files in .../ignite-go-client
